cf2bafebcaba1a570fa5ef9815799bdf75ac60a3
Xavier <facqueur.xavier@gmail.com>
2021-03-22 01:52:34 +0100

    add 2 new components for header and Card to customize element

---
+++ blokoding/components/Card.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import {View, StyleSheet} from 'react-native';
+
+const Card = props => {
+    return (
+        <View style={{...styles.inputContainer, ...props.style}}>
+            {props.children}
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    inputContainer:{
+        shadowColor:'black',
+        shadowOffset:{width:0, height:2},
+        shadowRadius:6,
+        shadowOpacity:0.26,
+        backgroundColor:'white',
+        padding:20,
+        borderRadius:10,
+    },
+})
+export default Card;
---
+++ blokoding/components/Header.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import {View, Text, StyleSheet, Image } from 'react-native'
+import Colors from '../constants/Colors'
+
+const Header = ({title}) => {
+    return (
+        <View style={styles.header}>
+            <Image
+          source={require("../assets/icon.png")}
+          style={{ width: 20, height: 20 }}
+          resizeMode="stretch"/>
+            <Text style={styles.headerTitle}>{title}</Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    header:{
+        width:1500,
+        height:57,
+        flexDirection:'row',
+        backgroundColor: Colors.primary,
+        alignItems: 'center',
+        justifyContent:'center',
+        borderBottomWidth:0.5,
+        borderBottomColor:'gray'
+    },
+    headerTitle:{
+        color:'black',
+        fontSize:18,
+    },
+})
+
+export default Header;
