1a4a4f4048cca2ca21e0c5278f8e16e553fb6d4d
Xavier <facqueur.xavier@gmail.com>
2021-03-24 17:22:19 +0100

    App.js modifications

--- blokoding/App.js
+++ blokoding/App.js
@@ -14,27 +14,15 @@ import {
   SafeAreaView,
   StyleSheet,
   ScrollView,
-  View,
-  Image,
-  Text,
-  StatusBar,
   Platform,
   AppRegistry,
   TouchableOpacity,
   Linking,
-  Button
 } from 'react-native';
 import SplashScreen from 'react-native-splash-screen';
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-import { RNCamera } from 'react-native-camera';
-import RNTextDetector from "rn-text-detector";
-
+import Header from './components/Header';
+import Home from './screens/HomeScreen';
+import Camera from './screens/CameraScreen';
 
 const Stack = createStackNavigator();
 
@@ -46,85 +34,15 @@ function App() {
 
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={Home} />
-        <Stack.Screen name="Take Picture" component={Blokoding}  screenOptions={{ headerShown: false }}/>
+      <Stack.Navigator screenOptions={{ headerShown: true, headerTitleAlign:'center' }}>
+        <Stack.Screen name="Home" component={Home} options={{headerTitle: () => <Header title="BLOKODING"/>}}/>
+        <Stack.Screen name="Take Picture" component={Camera} options={{headerShown: false}}/>
       </Stack.Navigator>
     </NavigationContainer>
   );
 };
 
-const Home = ({ navigation }) => {
-  return (
-    <View style={{
-      justifyContent: 'center',
-      alignItems: 'center' }}>
-      <Image
-        source={require("./assets/icon.png")}
-        style={{ width: 200, height: 200}}
-        resizeMode="stretch"/>
-      <Button
-        title="Start"
-        color="#841584"
-        onPress={() =>
-          navigation.navigate('Take Picture')}/>
-      <StatusBar style="auto" />
-    </View>
-  );
-};
-
-class Blokoding extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <RNCamera style={styles.preview} ref={ref => {this.camera = ref;}} type={RNCamera.Constants.Type.back} flashMode={RNCamera.Constants.FlashMode.off} captureAudio={false} />
-        <View style={{ flex: 0, flexDirection: 'row', justifyContent: 'center' }}>
-          <TouchableOpacity onPress={this.detectText} style={styles.capture}>
-            <Text style={{ fontSize: 14 }}>Process</Text>
-          </TouchableOpacity>
-        </View>
-      </View>
-    )
-  }
-
-  detectText = async () => {
-    try {
-      const options = {
-        quality: 0.8,
-        base64: true,
-        //skipProcessing: true,
-      };
-      const { uri } = await this.camera.takePictureAsync(options);
-      const visionResp = await RNTextDetector.detectFromUri(uri);
-      visionResp.forEach(element => {
-        console.log(element.text);
-      });
-    } catch (e) {
-      console.warn(e);
-    }
-  };
-}
-
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    flexDirection: 'column',
-    backgroundColor: 'black',
-  },
-  preview: {
-    flex: 1,
-    justifyContent: 'flex-end',
-    alignItems: 'center',
-  },
-  capture: {
-    flex: 0,
-    backgroundColor: '#fff',
-    borderRadius: 5,
-    padding: 15,
-    paddingHorizontal: 20,
-    alignSelf: 'center',
-    margin: 20,
-  },
 });
 
 export default App;
