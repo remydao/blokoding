e67806caddace1734a616e63596db649665117c7
Xavier <facqueur.xavier@gmail.com>
2021-04-11 15:58:41 +0200

    set gameScreen component

--- blokoding/screens/GameScreen.js
+++ blokoding/screens/GameScreen.js
@@ -1,45 +1,26 @@
 import React, {Component} from 'react';
-import {View, Text, Image, Button, StatusBar, StyleSheet} from 'react-native';
-import { useEffect, useState } from 'react/cjs/react.development';
-import {characterImages} from "../constants/CharacterImages";
+import {View, Text, Image, Button, StatusBar, StyleSheet, ImageBackground} from 'react-native';
 import Matter from "matter-js";
-import { GameEngine } from "react-native-game-engine"
+import { GameEngine } from "react-native-game-engine";
+import Character from "../components/Character";
+import BackgroundGame from "../components/BackgroundGame";
 
-class Game extends Component {
+export default class Game extends Component {
+
+    constructor(props) {
+        super(props);
+    
+        console.log(this.props.route.params.visionResp[0].text)
+    }
+
     render(){
-        let imageSource = "";
-        function findCharacter(name){
-            switch(name){
-                case 'Bart':
-                    imageSource = characterImages.Bart.uri;
-                    break;
-                case 'Charlie':
-                    imageSource = characterImages.Charlie.uri;
-                    break;
-                case 'Cyclops':
-                    imageSource = characterImages.Cyclops.uri;
-                    break;
-                case 'Dinny':
-                    imageSource = characterImages.Dinny.uri;
-                    break;
-                case 'Harry':
-                    imageSource = characterImages.Harry.uri;
-                    break;
-                case 'Kevin':
-                    imageSource = characterImages.Kevin.uri;
-                    break;
-            }
-        }
-        findCharacter(this.props.route.params.visionResp[0].text);
-
-        
         return (
-            <GameEngine style={{ width: 800, height: 600, backgroundColor: "blue" }}
+            <GameEngine
+                style={{flex:1}}
+                systems={[]}
                 entities={{
-                    initialBox: {
-                        body: initialBox, 
-                        size: [200, 200], 
-                    }
+                    1: { position: [0,0], renderer: <BackgroundGame/> },
+                    2: { position: [0, 500], characterName:this.props.route.params.visionResp[0].text, renderer: <Character/> },
                 }}>
             <StatusBar hidden={true} />
             </GameEngine>
@@ -49,9 +30,6 @@ class Game extends Component {
 
 const styles = StyleSheet.create({
     container:{
-        justifyContent:"center",
-        alignItems:"center"
+        flex:1
     }
 })
-
-export default Game;
