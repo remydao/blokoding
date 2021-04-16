642b3e32bc9f7453372e459c241dd8295f4802bf
Xavier <facqueur.xavier@gmail.com>
2021-04-16 12:23:34 +0200

    Modal error

--- blokoding/screens/CameraScreen.js
+++ blokoding/screens/CameraScreen.js
@@ -1,21 +1,51 @@
 import 'react-native-gesture-handler';
 import React, {Component} from 'react';
 import {StatusBar} from 'react-native';
-import { SafeAreaView, StyleSheet, ScrollView, View, Text, TouchableOpacity, Button } from 'react-native';
+import { SafeAreaView, StyleSheet, ScrollView, View, Text, TouchableOpacity, Button, Modal, Pressable } from 'react-native';
 import { RNCamera } from 'react-native-camera';
 import RNTextDetector from "rn-text-detector";
 import CharacterImages, { characterImages } from "../constants/CharacterImages";
 
 
 class Camera extends Component {
+    state = {
+      modalVisible: false
+    }
+
+    setModalVisible = (visible) => {
+      this.setState({modalVisible: visible})
+    }
+
     render() {
+      const {modalVisible} = this.state;
       return (
         <View style={styles.container}>
+          
           <RNCamera style={styles.preview} ref={ref => {this.camera = ref;}} type={RNCamera.Constants.Type.back} flashMode={RNCamera.Constants.FlashMode.off} captureAudio={false} />
           <View style={{ flex: 0, flexDirection: 'row', justifyContent: 'center' }}>
             <TouchableOpacity onPress={() => this.detectText(this.props.navigation, this.props.route.params.isSelectCharacter)} style={styles.capture}>
-              <Text style={{ fontSize: 14 }}>      </Text>
-            </TouchableOpacity>
+                <Text style={{ fontSize: 14 }}>      </Text>
+              </TouchableOpacity>
+          <Modal
+            animationType="slide"
+            transparent={true}
+            visible={modalVisible}
+            onRequestClose={() => {
+              this.setModalVisible(!modalVisible)
+            }}
+          >
+            <View style={styles.centeredView}>
+              <View style={styles.modalView}>
+                <Text style={styles.modalText}>Le scan a échoué, veuillez réessayer</Text>
+                  <Pressable
+                    style={[styles.button, styles.buttonClose]}
+                    onPress={() => this.setModalVisible(false)}
+                    >
+                    <Text style={styles.textStyle}>Fermer</Text>
+                  </Pressable>
+                </View>
+              </View>
+            </Modal>
           </View>
           <StatusBar translucent backgroundColor="transparent"/>
         </View>
@@ -36,18 +66,17 @@ class Camera extends Component {
         for (var character in characterImages){
           characterList.push(characterImages[character].imageName);
         }
-        if (isSelectCharacter){
-          if (visionResp.length == 0 || !characterList.includes(visionResp[0].text)){
-            navigation.navigate('Error', {visionResp : visionResp});
-          }
-          else
+        if (visionResp.length == 0 || !characterList.includes(visionResp[0].text)){
+          this.setModalVisible(!this.modalVisible)
+        }
+        else if (isSelectCharacter){
             navigation.navigate('Game', {visionResp: visionResp});
         }
         else{
           navigation.navigate('Result', {visionResp : visionResp});
         }
       } catch (e) {
-        //console.warn(e)
+        this.setModalVisible(!this.modalVisible)
       }
     };
   }
@@ -72,6 +101,44 @@ class Camera extends Component {
       alignSelf: 'center',
       bottom:30
     },
+    centeredView: {
+      flex: 1,
+      justifyContent: "center",
+      alignItems: "center",
+      marginTop: 22
+    },
+    modalView: {
+      margin: 20,
+      backgroundColor: "white",
+      borderRadius: 20,
+      padding: 35,
+      alignItems: "center",
+      shadowColor: "#000",
+      shadowOffset: {
+        width: 0,
+        height: 2
+      },
+      shadowOpacity: 0.25,
+      shadowRadius: 4,
+      elevation: 5
+    },
+    button: {
+      borderRadius: 20,
+      padding: 10,
+      elevation: 2
+    },
+    buttonClose: {
+      backgroundColor: 'red',
+    },
+    textStyle: {
+      color: "white",
+      fontWeight: "bold",
+      textAlign: "center"
+    },
+    modalText: {
+      marginBottom: 15,
+      textAlign: "center"
+    }
   });
 
   export default Camera;
