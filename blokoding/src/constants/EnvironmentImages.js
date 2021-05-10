42b8379b51f9eed4785f508c4190920cf733a1fb
KoHann <alexandre.vermeulen@epita.fr>
2021-05-11 00:51:25 +0200

    add item and environment assets and declare path refs

---
+++ blokoding/src/constants/EnvironmentImages.js
@@ -0,0 +1,27 @@
+import { Environments } from "./BlockType";
+
+
+const EnvironmentImages = {
+    Door: {
+        imageName: Environments.Door,
+        uri: require('../assets/environments/door.png')
+    },
+    Chair: {
+        imageName: Environments.Chair,
+        uri: require('../assets/environments/chair.png')
+    },
+    Puddle: {
+        imageName: Environments.Puddle,
+        uri: require('../assets/environments/puddle.png')
+    },
+    Bush: {
+        imageName: Environments.Bush,
+        uri: require('../assets/environments/bush.png')
+    },
+    Bin: {
+        imageName: Environments.Bin,
+        uri: require('../assets/environments/bin.png')
+    },
+}
+
+export default EnvironmentImages;
---
+++ blokoding/src/constants/ItemImages.js
@@ -0,0 +1,27 @@
+import { Items } from "./BlockType";
+
+
+const ItemImages = {
+    Key: {
+        imageName: Items.Key,
+        uri: require('../assets/items/key.png')
+    },
+    Plush: {
+        imageName: Items.Plush,
+        uri: require('../assets/items/plush.png')
+    },
+    Flower: {
+        imageName: Items.Flower,
+        uri: require('../assets/items/flower.png')
+    },
+    Machete: {
+        imageName: Items.Machete,
+        uri: require('../assets/items/machete.png')
+    },
+    Trash: {
+        imageName: Items.Trash,
+        uri: require('../assets/items/trash.png')
+    },
+}
+
+export default ItemImages;
