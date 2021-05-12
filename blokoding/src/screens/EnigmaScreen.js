5c98bd81b5e32d711431bebd40d1ec7b8e82250d
remydao <remy.dao.pro@gmail.com>
2021-05-12 22:52:19 +0200

    message box style commencé

--- blokoding/src/screens/EnigmaScreen.js
+++ blokoding/src/screens/EnigmaScreen.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
-import {View, StyleSheet} from 'react-native';
+import { ImageBackground } from 'react-native';
+import {View, StyleSheet, Image} from 'react-native';
 import TextAnimator from '../components/TextAnimator';
 
 
@@ -11,6 +12,7 @@ class EnigmaScreen extends Component {
     render() {
         return (
             <View style={styles.container}>
+                <Image source={require("../assets/empty_messageBox.png")} style={styles.theImage}></Image>
                 <TextAnimator content="Je suis un texte vraiment très long qui a pour but de tester le enigma screen.... Est ce que ca fonctionne ?"></TextAnimator>
             </View>
         );
@@ -21,9 +23,18 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         justifyContent: 'center',
-        backgroundColor: '#ecf0f1',
-        padding: 8
+        backgroundColor: '#cbcef8',
+        alignItems: 'center',
+        //padding: 30,
     },
+    theImage: {
+        width: "100%",
+        height: "50%",
+        //resizeMode: "contain",
+        //position: 'relative',
+        //top: -10,
+        //left: 70
+    }
 })
 
 export default EnigmaScreen;
