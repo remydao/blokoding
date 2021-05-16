28afe500a642727bd6cab547d45549981098f70c
Xavier <facqueur.xavier@gmail.com>
2021-05-16 15:52:13 +0200

    EnigmaScreen responsive

--- blokoding/src/screens/EnigmaScreen.js
+++ blokoding/src/screens/EnigmaScreen.js
@@ -1,20 +1,21 @@
 import React, {Component} from 'react';
 import { ImageBackground } from 'react-native';
-import {View, StyleSheet, Image, Text, Button, Animated, TouchableHighlight} from 'react-native';
+import {View, StyleSheet, Image, Text, Button, Animated, TouchableHighlight, SafeAreaView} from 'react-native';
 import TextAnimator from '../components/TextAnimator';
+import EngineConstants from '../constants/EngineConstants';
 
 
 class EnigmaScreen extends Component {
     constructor(props) {
         super(props);
-        this.content = ["personnage. Le personnage que tu choisiras sera celui qui s’affichera dans la scène et qui effectuera les actions que tu lui indiquera ensuite.Afin d’afficher Mr. Mustache, sélectionne sa carte personnage (rouge) et pose-la devant toi.Ensuite, clique sur le bouton ci-dessous et prend la carte en photo.", "Test 2 ou est tu ezuniueufez uezfui fe zuzefu zfeu zefuzeez hzfehuezfh ezhueepersonnage. Le personnage que tu choisiras sera celui qui s’affichera dans la scène et qui effectuera les actions que tu lui indiquera ensuite.Afin d’afficher Mr. Mustache, sélectionne sa carte personnage (rouge) et pose-la devant toi. "];
+        this.content = ["Le personnage que tu choisiras sera celui qui s’affichera dans la scène et qui effectuera les actions que tu lui indiquera ensuite.Afin d’afficher Mr. Mustache, sélectionne sa carte personnage (rouge) et pose-la devant toi.Ensuite, clique sur le bouton ci-dessous et prend la carte en photo.", "Test 2 ou est tu ezuniueufez uezfui fe zuzefu zfeu zefuzeez hzfehuezfh ezhueepersonnage. Le personnage que tu choisiras sera celui qui s’affichera dans la scène et qui effectuera les actions que tu lui indiquera ensuite.Afin d’afficher Mr. Mustache, sélectionne sa carte personnage (rouge) et pose-la devant toi. "];
         this.index = 0;
         this.state = {
             textAnimator: <TextAnimator content={this.content[0]}></TextAnimator>,
         }
     }
 
-    onPressButton() {
+    onPressButton = () => {
         console.log('Press Button')
         this.index++;
         this.setState({
@@ -22,36 +23,54 @@ class EnigmaScreen extends Component {
         });
     }
 
-    componentDidMount() {
-    }
 
     render() {
         return (
-                <View style={styles.container}>
-                    <ImageBackground source={require('../assets/empty_messageBox.png')} style={{width: '100%', height: '100%', position: 'relative', top: -70, right: -20}} resizeMode="contain">
-                        {this.state.textAnimator}
-                        <Image source={require('../assets/characters/Charlie/1x/Charlie.png')} style={{position: 'relative', bottom: -390, left: -30}}></Image>
-                    </ImageBackground>
-                    <Button
-                        onPress={this.onPressButton.bind(this)}
+                <SafeAreaView style={styles.container}>
+                    <Image source={require('../assets/empty_messageBox.png')} style={styles.popupImage} resizeMode="contain"/>
+                    <View style={styles.animatedText}>
+                            {this.state.textAnimator}
+                    </View>
+
+                    <Image source={require('../assets/characters/Charlie/1x/Charlie.png')} style={styles.image}></Image>
+                    {/* <Button
+                        onPress={this.onPressButton}
                         title="OK!"
                         color="#841584"
-                        style={{}}
-                    />
-                </View>
+                        style={styles.cameraBtn}
+                    /> */}
+                </SafeAreaView>
         );
     }
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        flexDirection: 'row',
-        justifyContent: 'space-around',
-        backgroundColor: '#cbcef8',
+    container:{
+        width: '100%',
+        height: '100%',
+        justifyContent: 'center',
         alignItems: 'center',
-        //padding: 30,
+        backgroundColor: 'purple'
     },
+    popupImage:{
+        position:'absolute',
+        width: EngineConstants.MAX_WIDTH,
+        height: EngineConstants.MAX_HEIGHT,
+    },
+    animatedText:{
+        position:'absolute',
+        justifyContent:'center',
+        alignItems: 'center',
+        paddingBottom: EngineConstants.MAX_HEIGHT / 7,
+    },
+    cameraBtn:{
+        position:'absolute'
+    },
+    image: {
+        position: 'absolute',
+        bottom: EngineConstants.MAX_HEIGHT / 7,
+        left: -EngineConstants.MAX_WIDTH / 30,
+    }
 })
 
 export default EnigmaScreen;
