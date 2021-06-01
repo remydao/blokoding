a9f53afe220086e2cf301f1e8fd2d76d6163b3be
KoHann <alexandre.vermeulen@epita.fr>
2021-06-01 19:16:43 +0200

    upgrade parsing errors explanation

--- src/scripts/parsing/Parser.ts
+++ src/scripts/parsing/Parser.ts
@@ -4,6 +4,7 @@ import { MoveBlock, JumpBlock, GrabBlock, SpeakBlock } from "../blocks/ActionBlo
 import { ForBlock, IfBlock, WhileBlock } from "../blocks/InstructionBlock";
 import { IsInFrontBlock, IsOnBlock, PossessBlock } from "../blocks/ConditionBlock";
 import { DataBlock } from "../blocks/DataBlock";
+import { ConditionBlock, StructureBlock } from "../blocks/MainBlocks";
 
 
 var loopDepth = 0;
@@ -26,7 +27,7 @@ const getFirstElm = (cardList: TcardList): string | undefined => {
     return cardList.shift();
 }
 
-const parseStructureCard = (cardList: TcardList) : never | MoveBlock | JumpBlock | GrabBlock | SpeakBlock | null => {
+const parseStructureCard = (cardList: TcardList) : never | StructureBlock | null => {
     let blockName = getFirstElm(cardList);
 
     if (typeof blockName === 'undefined') {
@@ -51,6 +52,19 @@ const parseStructureCard = (cardList: TcardList) : never | MoveBlock | JumpBlock
             return null;
         else
             throw 'Une carte fin est mal placée ' + cardIndexToString();
+    }
+
+    res = Object.entries(Conditions).filter(condition => condition[1] == blockName);
+    if (res.length > 0) {
+        throw 'La condition ' + res[0][1] + ' est mal placée ' + cardIndexToString();
+    }
+    res = Object.entries(Items).filter(item => item[1] == blockName);
+    if (res.length > 0) {
+        throw "L'objet " + res[0][1] + ' est mal placé ' + cardIndexToString();
+    }
+    res = Object.entries(Environments).filter(env => env[1] == blockName);
+    if (res.length > 0) {
+        throw "L'environnement " + res[0][1] + ' est mal placé ' + cardIndexToString();
     }
 
     throw 'Texte inconnu : "' + blockName + '" ' + cardIndexToString();
@@ -88,17 +102,20 @@ const parseAction = (action: any[], cardList: TcardList) => {
 }
 
 const parseInstruction = (instruction: any[], cardList: TcardList) => {
-    let predicateBlock: DataBlock | IsInFrontBlock | IsOnBlock;
+    let predicateBlock: DataBlock | ConditionBlock | null;
 
     switch (instruction[1]) {
         case Instructions.For:
-            predicateBlock = parseNumber(cardList);
+            if (!(predicateBlock = parseNumber(cardList)))
+                throw "L'instruction Répéter doit être suivie d'un nombre " + cardIndexToString();
             break;
         case Instructions.If:
-            predicateBlock = parseCondition(cardList);
+            if (!(predicateBlock = parseCondition(cardList)))
+                throw "L'instruction Si doit être suivie d'une condition " + cardIndexToString();            
             break;
         case Instructions.While:
-            predicateBlock = parseCondition(cardList);
+            if (!(predicateBlock = parseCondition(cardList)))
+                throw "L'instruction Tant que doit être suivie d'une condition " + cardIndexToString();            
             break;
         default:
             return null;
@@ -122,55 +139,67 @@ const parseInstruction = (instruction: any[], cardList: TcardList) => {
     }
 }
 
-const parseNumber = (cardList: TcardList): DataBlock | never => {
+const parseNumber = (cardList: TcardList): DataBlock | null => {
     let number = getFirstElm(cardList);
-    if (isNaN(number as any)) {
-        throw 'Il manque une carte nombre ' + cardIndexToString();
-    }
-    let n = parseInt(number || "");
+    if (!number)
+        return null;
+
+    let n = parseInt(number);
+    
+    if (isNaN(n)) 
+        return null;
+    
     return new DataBlock(n);
 }
 
-const parseCondition = (cardList: TcardList): IsInFrontBlock | IsOnBlock | never => {
+const parseCondition = (cardList: TcardList): ConditionBlock | null => {
     let conditionFirstCard = getFirstElm(cardList);
     let res = Object.entries(Conditions).filter(cond => cond[1] == conditionFirstCard);
+
+    let entity : DataBlock | null
 
     if (res.length > 0)
     {
         switch (res[0][1]) {
             case Conditions.IsInFront:
-                return new IsInFrontBlock(parseEnvironnement(cardList));
+                if (!(entity = parseEnvironnement(cardList)))
+                    throw "La condition être devant doit être suivie d'un environnement " + cardIndexToString();
+                return new IsInFrontBlock(entity);
             case Conditions.IsOn:
-                return new IsOnBlock(parseItem(cardList));
+                if (!(entity = parseItem(cardList)))
+                    throw "La condition être sur doit être suivie d'un objet " + cardIndexToString();
+                return new IsOnBlock(entity);
             case Conditions.Possess: 
-                return new PossessBlock(parseItem(cardList));
+                if (!(entity = parseItem(cardList)))
+                    throw "La condition posséder doit être suivie d'un objet " + cardIndexToString();
+                return new PossessBlock(entity);
             default:
-                throw 'Une carte condition est manquante ' + cardIndexToString();
+                return null;
         }
     }
     else
-        throw 'Une carte condition est manquante ' + cardIndexToString();
+        return null;
 }
 
-const parseItem = (cardList: TcardList): DataBlock | never => {
+const parseItem = (cardList: TcardList): DataBlock | null => {
     let item = getFirstElm(cardList);
     let res = Object.entries(Items).filter(it => it[1] == item);
 
     if (res.length > 0) {
         return new DataBlock(res[0][1]);
     } else {
-        throw 'Il manque une carte objet ' + cardIndexToString();
+        return null;
     }
 }
 
-const parseEnvironnement = (cardList: TcardList): DataBlock | never => {
+const parseEnvironnement = (cardList: TcardList): DataBlock | null => {
     let env = getFirstElm(cardList);
     let res = Object.entries(Environments).filter(e => e[1] == env);
 
     if (res.length > 0) {
         return new DataBlock(res[0][1]);
     } else {
-        throw 'Il manque une carte environnement ' + cardIndexToString();
+        return null;
     }
 }
 
