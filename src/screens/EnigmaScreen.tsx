d68ee838c9b30921e187833c7eec327de39046d1
Xavier <facqueur.xavier@gmail.com>
2021-07-05 18:43:22 +0200

    New screen with Enigma list

--- src/screens/EnigmaScreen.tsx
+++ src/screens/EnigmaScreen.tsx
@@ -1,79 +1,86 @@
-import React, {Component} from 'react';
-import {View, StyleSheet, Image, SafeAreaView} from 'react-native';
-import TextAnimator from '../components/TextAnimator';
-import EngineConstants from '../constants/EngineConstants';
+import React, {useState, useEffect, useReducer} from 'react';
+import {Text, View, StatusBar, StyleSheet, FlatList, Pressable, Animated} from 'react-native';
+import {default as UUID} from "uuid"; 
+import EnigmaButton from '../components/EnigmaButton';
+import Colors from '../constants/Colors';
+import EnigmaLevelScreen from './EnigmaLevelScreen';
+
+const DiscoverColors = [
+  Colors.dark_pink,
+  Colors.turquoise,
+  Colors.dark_red,
+  Colors.primary,
+  Colors.dark_purple,
+]
+
+
+interface IProps {
+  navigation: any,
+  route: any
+}
 
 interface IState {
-    textAnimator: JSX.Element
+  id: string,
+  value: string
 }
+//extends React.Component<IProps, IState>
+const EnigmaScreen = ({navigation, route}: IProps) => {
+    const [buttons, setButtons] = useState<IState[]>([]);
 
-class EnigmaScreen extends Component<{}, IState> {
-    private content : Array<any> = ["Le personnage que tu choisiras sera celui qui s’affichera dans la scène et qui effectuera les actions que tu lui indiquera ensuite.Afin d’afficher Mr. Mustache, sélectionne sa carte personnage (rouge) et pose-la devant toi.Ensuite, clique sur le bouton ci-dessous et prend la carte en photo.", "Test 2 ou est tu ezuniueufez uezfui fe zuzefu zfeu zefuzeez hzfehuezfh ezhueepersonnage. Le personnage que tu choisiras sera celui qui s’affichera dans la scène et qui effectuera les actions que tu lui indiquera ensuite.Afin d’afficher Mr. Mustache, sélectionne sa carte personnage (rouge) et pose-la devant toi. "];
-    private index : number = 0
-
-    constructor(props: {}) {
-        super(props);
-        this.state = {
-            textAnimator: <TextAnimator content={this.content[0]}></TextAnimator>,
-        }
+    const setLevel = () => {
+      let btns = [...buttons];
+      //numberOfLevels
+      for (let i = 0; i < 30; i++)
+      {
+          const levelTitle = "Niveau " + (i + 1)
+          btns.push({id:UUID.v4(), value:levelTitle})
+      }
+      setButtons(btns);
     }
 
-    onPressButton = () => {
-        console.log('Press Button')
-        this.index++;
-        this.setState({
-            textAnimator: <TextAnimator content={this.content[this.index]}></TextAnimator>
-        });
+    useEffect(() => {
+      setLevel();
+    }, [])
+
+    const onPress = (index: number) => {
+      navigation.navigate('EnigmaLevelScreen')
+    }
+
+    const selectBgColor = (index: number) => {
+      return DiscoverColors[index % DiscoverColors.length];
     }
 
 
-    render() {
-        return (
-                <SafeAreaView style={styles.container}>
-                    <Image source={require('../assets/empty_messageBox.png')} style={styles.popupImage} resizeMode="contain"/>
-                    <View style={styles.animatedText}>
-                            {this.state.textAnimator}
-                    </View>
-
-                    <Image source={require('../assets/characters/Charlie/1x/Charlie.png')} style={styles.image}></Image>
-                    {/* <Button
-                        onPress={this.onPressButton}
-                        title="OK!"
-                        color="#841584"
-                        style={styles.cameraBtn}
-                    /> */}
-                </SafeAreaView>
-        );
-    }
-}
-
-const styles = StyleSheet.create({
-    container:{
-        width: '100%',
-        height: '100%',
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: 'purple'
-    },
-    popupImage:{
-        position:'absolute',
-        width: EngineConstants.MAX_WIDTH,
-        height: EngineConstants.MAX_HEIGHT,
-    },
-    animatedText:{
-        position:'absolute',
-        justifyContent:'center',
-        alignItems: 'center',
-        paddingBottom: EngineConstants.MAX_HEIGHT / 7,
-    },
-    cameraBtn:{
-        position:'absolute'
-    },
-    image: {
-        position: 'absolute',
-        bottom: EngineConstants.MAX_HEIGHT / 7,
-        left: -EngineConstants.MAX_WIDTH / 30,
-    }
-})
+      const y = new Animated.Value(0);
+      const onScroll = Animated.event([{nativeEvent: {contentOffset: { y } } }], {
+        useNativeDriver: true
+      })
+      return (
+        <View style={styles.container}>
+            <View>
+              <Animated.FlatList
+                  scrollEventThrottle={16}
+                  data={buttons}
+                  renderItem={({item, index}) => 
+                    (  
+                        //<View style={{backgroundColor: this.selectBgColor(index)}}>
+                        <EnigmaButton bgColor={selectBgColor(index)} index={index} y={y} text={item.value} onPress={() => onPress(index)} pressColor={'red'}/>
+                        //</View>
+                      )}
+                  keyExtractor={item => item.id}
+                  {...{onScroll}}
+              />
+            </View>
+            <StatusBar backgroundColor={'purple'}/>
+        </View>
+      );
+  };
 
 export default EnigmaScreen;
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: 'purple'
+    },
+})
